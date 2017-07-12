a63c30cdcb19dc491428b62f65dbc8b1efa0836b
Felipe Salgado <felipe.salgado@transvip.cl>
2017-07-12 05:13:28 -0400

    I've added the second API's endpoint in order to process the GET requests.

--- app.js
+++ app.js
@@ -36,6 +36,37 @@ app.post('/api/address', function(req, res)
   }
 });
 
+/*
+* Endpoint in order to process list the stored addresses,
+* @param id = place_id (string), *optional*
+*/
+app.get('/api/address/:id?', function(req, res)
+{
+  // If the user does not specify a number of records to list, the system
+  // will show all of them.
+  var _query = (req.params.id === undefined) ? {} : { id: req.params.id };
+
+  // Getting the addresses from the database,
+  // { _id:0, __v:0 } is for to exclude that fields of the result.
+  Address.find(_query, { _id:0, __v:0 }, function(err, addresses)
+  {
+    if(err)
+    {
+      var _errorGetting = "Error getting addresses from database.";
+      logger.log(_errorGetting);
+      res.json(_errorGetting)
+    }
+    else
+    {
+      // If there is not addresses related to that id (place_id),
+      // the system shows the proper answer.
+      var _queryResult = (addresses.length === 0) ? { message: "There is not records related to that place id." } : addresses;
+      logger.log("Query performed successfully.");
+      res.json(_queryResult);
+    }
+  });
+});
+
 app.listen(port, function()
 {
   console.log("The API has started successfully on port "+port);
